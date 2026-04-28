4874e712f279c5f4fafeb6a4214f364b91a80ad0
buse506 <032290055@ogr.uludag.edu.tr>
2026-04-28 19:55:36 +0300

    revizyon

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from huffman import HuffmanCodec
 
 def test_huffman():
-    # 1. Önce bir test dosyası oluşturalım (veya var olanı kullanalım)
+    
     test_file = "test_metni.txt"
     mesaj = "bursa uludag universitesi bilgisayar muhendisligi"
     
@@ -12,23 +12,21 @@ def test_huffman():
     print(f"Orijinal Metin: {mesaj}")
     print(f"Orijinal Boyut: {len(mesaj) * 8} bit (ASCII)")
 
-    # 2. Huffman Nesnesini Oluştur
+    
     codec = HuffmanCodec(test_file)
     
-    # 3. Encode (Sıkıştırma) İşlemi
-    # Bu metodun içinde frekans sayma, ağaç kurma ve kod üretme hepsi dönüyor
+    
     encoded_mesaj = codec.encode()
     
     print(f"\n--- SIKIŞTIRMA SONUCU ---")
     print(f"Kodlanmış (Bit) Hali: {encoded_mesaj}")
     print(f"Yeni Boyut: {len(encoded_mesaj)} bit")
     
-    # Sıkıştırma oranını hesaplayalım
+    
     oran = (1 - (len(encoded_mesaj) / (len(mesaj) * 8))) * 100
     print(f"Sıkıştırma Oranı: %{oran:.2f}")
 
-    # 4. Decode (Geri Açma) İşlemi
-    # build_huffman_tree'den dönen root'u burada kullanıyoruz
+    
      
     decoded_mesaj = codec.decode(encoded_mesaj)
     
@@ -36,11 +34,11 @@ def test_huffman():
     print(f"Çözülen Metin: {decoded_mesaj}")
 
    
-    # 5. Doğrulama
+    
     if mesaj == decoded_mesaj:
         print("\nBASARILI: Mesaj kayipsiz bir sekilde geri donduruldu!")
     else:
         print("\nHATA: Mesajlar uyusmuyor, agac yapisini kontrol et.")
-# Çalıştır kanka!
+
 if __name__ == "__main__":
     test_huffman()
